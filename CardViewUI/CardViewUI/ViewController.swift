4f46c52350a508467bf616d29290ab9989d5dedb
mrCrambo <mrcrambo@mrCrambos-MacBook-Pro.local>
2019-10-05 00:09:45 +0300

    ui with shadow and rounded border

---
+++ CardViewUI/CardViewUI/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CardViewUI
+//
+//  Created by mrCrambo on 04/10/2019.
+//  Copyright © 2019 mrCrambo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       }
+    
+}
+
+class ShadowView: UIView {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        // shadow
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowOpacity = 0.2
+        self.layer.shadowRadius = 2.0
+    }
+
+}
