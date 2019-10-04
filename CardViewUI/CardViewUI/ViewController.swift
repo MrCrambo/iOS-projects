bd08340a6d50bef0755f0a9581ebd69824967826
MrCrambo <kadirov.il.crambo@gmail.com>
2019-10-05 00:19:42 +0300

    Update ViewController.swift

--- CardViewUI/CardViewUI/ViewController.swift
+++ CardViewUI/CardViewUI/ViewController.swift
@@ -21,8 +21,6 @@ class ShadowView: UIView {
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-
-        // shadow
         self.layer.masksToBounds = false
         self.layer.shadowColor = UIColor.black.cgColor
         self.layer.shadowOffset = CGSize(width: 0, height: 0)
