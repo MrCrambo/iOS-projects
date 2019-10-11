fba2ec20d3539a3112b576c67a5af8be14af5f47
MrCrambo <kadirov.il.crambo@gmail.com>
2019-10-11 19:44:48 +0300

    initial commit

---
+++ FaceMarks/FaceMarks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceMarks
+//
+//  Created by mrCrambo on 11/10/2019.
+//  Copyright © 2019 mrCrambo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
