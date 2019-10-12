6d667d877ffe696c2075e58288d0f332e4f9fd4e
MrCrambo <kadirov.il.crambo@gmail.com>
2019-10-12 13:17:04 +0300

    image clickers added

--- FaceMarks/FaceMarks/ViewController.swift
+++ FaceMarks/FaceMarks/ViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var firstPhoto: UIImageView!
+    @IBOutlet weak var secondPhoto: UIImageView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        firstPhoto.isUserInteractionEnabled = true
+        firstPhoto.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(firstImageTapped(_:))))
+        
+        secondPhoto.isUserInteractionEnabled = true
+        secondPhoto.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(secondImageTapped(_:))))
     }
 
+    @objc private func firstImageTapped(_ recognizer: UITapGestureRecognizer) {
+        print("first image tapped")
+    }
+    
+    @objc private func secondImageTapped(_ recognizer: UITapGestureRecognizer) {
+        print("second image tapped")
+    }
 
 }
 
