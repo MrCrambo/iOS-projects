884a0514853d788a05d28464625b764e4ffde1f4
MrCrambo <kadirov.il.crambo@gmail.com>
2019-11-06 23:54:45 +0300

    ar first project added

---
+++ AR one/AR one/GameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GameViewController.swift
+//  AR one
+//
+//  Created by mrcrambo on 06/11/2019.
+//  Copyright © 2019 mrcrambo. All rights reserved.
+//
+
+import ARKit
+
+class GameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ AR one/AR one/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR one
+//
+//  Created by mrcrambo on 06/11/2019.
+//  Copyright © 2019 mrcrambo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
